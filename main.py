e9224ead90f6c98c9a811dfccca21023cf36ef7f
jaimeanchique <87664599+jaimeanchique@users.noreply.github.com>
2024-08-24 20:55:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,15 +13,20 @@ def api_predict():
         return "PLEASE Send POST request"
     elif request.method == "POST":
 
-        print("Hello" + str(request.get_json()))
+        //print("Hello" + str(request.get_json()))
+        //data = request.get_json()
 
-        data = request.get_json()
+        //sepal_length = data['sepal_length']
+        //sepal_width = data['sepal_width']
+        //petal_length = data['petal_length']
+        //pepal_width = data['petal_width']
 
-        sepal_length = data['sepal_length']
-        sepal_width = data['sepal_width']
-        petal_length = data['petal_length']
-        pepal_width = data['petal_width']
+        sepal_length = request.form.get('sepal_length')
+        sepal_width = request.form.get('sepal_width')
+        petal_length = request.form.get('petal_length')
+        petal_width = request.form.get('petal_width')
 
+        
         in1 = np.array([[sepal_length, sepal_width, petal_length, pepal_width]])
 
         prediction = model_pk.predict(in1)
